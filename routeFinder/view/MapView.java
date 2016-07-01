4dae01fa1c22e93a9c42446c63e2e7209143cd4e
gibsonhladky <hladky@wisc.edu>
2016-07-01 13:37:31 -0500

    Removed old functionality from MapView
    Methods that were moved previously were deleted.

--- routeFinder/view/MapView.java
+++ routeFinder/view/MapView.java
@@ -12,10 +12,6 @@ public class MapView {
 	private PApplet applet;
 	private Map map;
 	
-	private Point guiStart;
-	private Point guiEnd;
-	private Point guiDragging;
-	
 	private final int height, width;
 
 	public MapView(Map map, PApplet p) {
@@ -23,52 +19,6 @@ public class MapView {
 		applet = p;
 		height = applet.height;
 		width = applet.width;
-		initializeGuiPoints();
-	}
-	
-	/*
-	 * Sets the start and end GUI points to an initial state. The points are set
-	 * apart to allow easier usability.
-	 */
-	private void initializeGuiPoints() {
-		guiStart = new Point(applet.width * 2 / 10, applet.height / 2);
-		guiEnd = new Point(applet.width * 8 / 10, applet.height / 2);
-		guiDragging = null;
-		moveEndPointsToClosestStreet();
-	}
-
-	private void moveEndPointsToClosestStreet() {
-		map.moveEndPointsToClosestStreet(guiStart, guiEnd);
-	}
-
-	/*
-	 * Updates the position of the start and end points.
-	 * Allows the user to drag the points to new positions.
-	 * Draws the points after being called.
-	 */
-	public void updateStartAndEndPoints() {
-		// check for mouse press over start and end locations
-		if (applet.mousePressed && guiDragging == null) {
-			float dToStartSqr = (float) ( sqr(applet.mouseX - guiStart.x) + sqr(applet.mouseY - guiStart.y) );
-			float dToEndSqr = (float) ( sqr(applet.mouseX - guiEnd.x) + sqr(applet.mouseY - guiEnd.y) );
-			if (dToStartSqr <= 50 && dToStartSqr < dToEndSqr) {
-				guiDragging = guiStart;
-			}
-			else if (dToEndSqr <= 50) {
-				guiDragging = guiEnd;
-			}
-		}
-		// dragging start or end location
-		if (applet.mousePressed & ( guiDragging != null )) {
-			guiDragging.x = applet.mouseX;
-			guiDragging.y = applet.mouseY;
-		}
-		// stop dragging start or end location
-		if (!applet.mousePressed && ( guiDragging != null )) {
-			map.moveEndPointsToClosestStreet(guiStart, guiEnd);
-			guiDragging = null;
-		}
-
 	}
 
 	/*
@@ -133,12 +83,5 @@ public class MapView {
 		applet.ellipse(guiStart.x, guiStart.y, 8, 8);
 		applet.stroke(255, 0, 0);
 		applet.ellipse(guiEnd.x, guiEnd.y, 8, 8);
-	}
-	
-	/*
-	 * Square function. Returns x^2
-	 */
-	private double sqr(double x) {
-		return Math.pow(x, 2);
 	}
 }
