b152dddf37581d712822f0c8883552afab7fac3b
gibsonhladky <hladky@wisc.edu>
2016-07-08 13:46:16 -0500

    Refactored loadPoints in MapLoader

--- routeFinder/model/MapLoader.java
+++ routeFinder/model/MapLoader.java
@@ -2,7 +2,6 @@ package routeFinder.model;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
-import java.util.List;
 
 import processing.data.XML;
 
@@ -12,39 +11,39 @@ import processing.data.XML;
  * about the map.
  */
 public class MapLoader {
-	
+
 	private XML mapData;
 	private Bounds bounds;
 	private final Hashtable<Long, Point> pointIDTable = new Hashtable<Long, Point>();
 	private ArrayList<Point> points;
 	private ArrayList<Street> streets;
-	
+
 	private final int width, height;
-	
+
 	public MapLoader(int width, int height) {
 		this.width = width;
 		this.height = height;
 	}
-	
+
 	public Map loadMap(XML mapData) {
 		this.mapData = mapData;
 		load();
 		return new Map(points, streets, bounds, width, height);
 	}
-	
+
 	private void load() {
 		loadBounds();
 		loadPoints();
 		loadStreets();
 		removeUnusedPoints();
 	}
-	
+
 	private void loadBounds() {
 		XML boundsData = mapData.getChild("bounds");
-		bounds = new Bounds(boundsData.getFloat("minlat"), boundsData.getFloat("minlon"),
-				boundsData.getFloat("maxlat"), boundsData.getFloat("maxlon"));
+		bounds = new Bounds(boundsData.getFloat("minlat"), boundsData.getFloat("minlon"), boundsData.getFloat("maxlat"),
+				boundsData.getFloat("maxlon"));
 	}
-	
+
 	/*
 	 * Loads all the points from the XML mapData.
 	 */
@@ -52,48 +51,56 @@ public class MapLoader {
 		points = new ArrayList<Point>();
 		XML nodes[] = mapData.getChildren("node");
 		for (XML node : nodes) {
-			// Filter out invalid nodes
-			if (!node.hasAttribute("id") || !node.hasAttribute("lat") || !node.hasAttribute("lon")) {
+			if (!isValidPoint(node)) {
 				continue;
 			}
-			
-			long id = node.getLong("id", -1);
-			float lat = node.getFloat("lat");
-			float lon = node.getFloat("lon");
-			Point point = new Point(scaleLon(lon), scaleLat(lat));
-			points.add(point);
-			pointIDTable.put(id, point);
+
+			loadPoint(node);
 		}
 	}
-	
+
+	private boolean isValidPoint(XML node) {
+		return !node.hasAttribute("id") || !node.hasAttribute("lat") || !node.hasAttribute("lon");
+	}
+
+	private void loadPoint(XML node) {
+		long id = node.getLong("id", -1);
+		float lat = node.getFloat("lat");
+		float lon = node.getFloat("lon");
+		Point point = new Point(scaleLon(lon), scaleLat(lat));
+		points.add(point);
+		pointIDTable.put(id, point);
+	}
+
 	/*
 	 * Scales the longitude to fit the screen.
 	 */
 	private float scaleLon(float lon) {
 		return width * ( lon - bounds.minLon ) / bounds.lonRange;
 	}
-	
+
 	/*
 	 * Scales the latitude to fit the screen.
 	 */
 	private float scaleLat(float lat) {
 		return height - height * ( lat - bounds.minLat ) / bounds.latRange;
 	}
-	
+
 	/*
-	 * Loads the streets in a map from the XML mapData.
-	 * Must be called after loadPoints, as streets are
-	 * constructed by connecting Points.
+	 * Loads the streets in a map from the XML mapData. Must be called after
+	 * loadPoints, as streets are constructed by connecting Points.
 	 */
 	private void loadStreets() {
 		streets = new ArrayList<Street>();
+		
+		// 
 		XML ways[] = mapData.getChildren("way");
 		for (XML way : ways) {
 			// read road type and name
 			boolean isRoad = false;
 			String name = "";
 			XML tags[] = way.getChildren("tag");
-			for (XML tag : tags)
+			for (XML tag : tags) {
 				if (tag.hasAttribute("k") && tag.hasAttribute("v")) {
 					if (tag.getString("k").equals("highway")) {
 						switch (tag.getString("v")) {
@@ -113,8 +120,10 @@ public class MapLoader {
 						name = tag.getString("v");
 					}
 				}
-			if (!isRoad)
+			}
+			if (!isRoad) {
 				continue;
+			}
 			// read list of points
 			ArrayList<Point> points = new ArrayList<Point>();
 			XML nds[] = way.getChildren("nd");
@@ -140,7 +149,7 @@ public class MapLoader {
 			}
 		}
 	}
-	
+
 	private void removeUnusedPoints() {
 		for (int i = 0; i < points.size(); i++) {
 			if (!points.get(i).isOnStreet) {
