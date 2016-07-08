ff4aef9e8843595f148c5c27f9b0b27bb06d5a83
gibsonhladky <hladky@wisc.edu>
2016-07-08 14:15:25 -0500

    Simplified loadPoints and loadStreets
    Extracted methods from both methods to simplify their implementation
    and clarify the process they follow.

--- routeFinder/model/MapLoader.java
+++ routeFinder/model/MapLoader.java
@@ -52,18 +52,24 @@ public class MapLoader {
 		points = new ArrayList<Point>();
 		XML nodes[] = mapData.getChildren("node");
 		for (XML node : nodes) {
-			// Filter out invalid nodes
-			if (!node.hasAttribute("id") || !node.hasAttribute("lat") || !node.hasAttribute("lon")) {
+			if (isInvalidNode(node)) {
 				continue;
 			}
-			
-			long id = node.getLong("id", -1);
-			float lat = node.getFloat("lat");
-			float lon = node.getFloat("lon");
-			Point point = new Point(scaleLon(lon), scaleLat(lat));
-			points.add(point);
-			pointIDTable.put(id, point);
+			loadPoint(node);
 		}
+	}
+	
+	private boolean isInvalidNode(XML node) {
+		return !node.hasAttribute("id") || !node.hasAttribute("lat") || !node.hasAttribute("lon");
+	}
+	
+	private void loadPoint(XML node) {
+		long id = node.getLong("id", -1);
+		float lat = node.getFloat("lat");
+		float lon = node.getFloat("lon");
+		Point point = new Point(scaleLon(lon), scaleLat(lat));
+		points.add(point);
+		pointIDTable.put(id, point);
 	}
 	
 	/*
@@ -87,57 +93,71 @@ public class MapLoader {
 	 */
 	private void loadStreets() {
 		streets = new ArrayList<Street>();
+		
+		// Go through each potential street
 		XML ways[] = mapData.getChildren("way");
-		for (XML way : ways) {
-			// read road type and name
-			boolean isRoad = false;
-			String name = "";
-			XML tags[] = way.getChildren("tag");
-			for (XML tag : tags)
-				if (tag.hasAttribute("k") && tag.hasAttribute("v")) {
-					if (tag.getString("k").equals("highway")) {
-						switch (tag.getString("v")) {
-						case "pedestrian":
-						case "footway":
-						case "cycleway":
-						case "steps":
-						case "path":
-						case "living street":
-							// isRoad = false; // initialized to false above
-							break;
-						default:
-							isRoad = true;
-						}
-					}
-					else if (tag.getString("k").equals("name")) {
-						name = tag.getString("v");
+		for (XML road : ways) {
+			if (!isRoad(road)) {
+				continue;
+			}
+			
+			Street newStreet = createStreet(road);
+			streets.add(newStreet);
+			setNeighbors(newStreet);
+		}
+	}
+	
+	private boolean isRoad(XML way) {
+		boolean isRoad = false;
+		XML tags[] = way.getChildren("tag");
+		for (XML tag : tags) {
+			if (tag.hasAttribute("k") && tag.hasAttribute("v")) {
+				if (tag.getString("k").equals("highway")) {
+					switch (tag.getString("v")) {
+					case "pedestrian":
+					case "footway":
+					case "cycleway":
+					case "steps":
+					case "path":
+					case "living street":
+						// isRoad = false; // initialized to false above
+						break;
+					default:
+						isRoad = true;
 					}
 				}
-			if (!isRoad)
-				continue;
-			// read list of points
-			ArrayList<Point> points = new ArrayList<Point>();
-			XML nds[] = way.getChildren("nd");
-			for (XML nd : nds) {
-				long index = nd.getLong("ref", -1);
-				Point nextPoint = pointIDTable.get(index);
-				nextPoint.isOnStreet = true;
-				points.add(nextPoint);
+				else if (tag.getString("k").equals("name")) {
+					
+				}
 			}
-			// create new street
-			Street street = new Street(points, name);
-			streets.add(street);
-			// add neighboring nodes to each node
-			if (points.size() > 1) {
-				points.get(0).neighbors.add(points.get(1));
-			}
-			for (int i = 1; i < points.size() - 1; i++) {
-				points.get(i).neighbors.add(points.get(i - 1));
-				points.get(i).neighbors.add(points.get(i + 1));
-			}
-			if (points.size() > 1) {
-				points.get(points.size() - 1).neighbors.add(points.get(points.size() - 2));
-			}
+		}
+		return isRoad;
+	}
+	
+	private Street createStreet(XML road) {
+		ArrayList<Point> points = new ArrayList<Point>();
+		XML nds[] = road.getChildren("nd");
+		for (XML nd : nds) {
+			long index = nd.getLong("ref", -1);
+			Point nextPoint = pointIDTable.get(index);
+			nextPoint.isOnStreet = true;
+			points.add(nextPoint);
+		}
+		
+		return new Street(points, "");
+	}
+	
+	private void setNeighbors(Street street) {
+		ArrayList<Point> newPoints = street.points;
+		if (newPoints.size() > 1) {
+			newPoints.get(0).neighbors.add(newPoints.get(1));
+		}
+		for (int i = 1; i < newPoints.size() - 1; i++) {
+			newPoints.get(i).neighbors.add(newPoints.get(i - 1));
+			newPoints.get(i).neighbors.add(newPoints.get(i + 1));
+		}
+		if (newPoints.size() > 1) {
+			newPoints.get(newPoints.size() - 1).neighbors.add(newPoints.get(newPoints.size() - 2));
 		}
 	}
 	
