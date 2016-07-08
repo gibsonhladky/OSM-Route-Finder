c3a831da7a6d95727ab15989023846893985e499
gibsonhladky <hladky@wisc.edu>
2016-07-08 13:47:19 -0500

    Removed name field in Street
    Street was equipped with a name field that was never used in the
    project. It was removed, but may be reimplemented later.

--- routeFinder/model/MapLoader.java
+++ routeFinder/model/MapLoader.java
@@ -134,7 +134,7 @@ public class MapLoader {
 				points.add(nextPoint);
 			}
 			// create new street
-			Street street = new Street(points, name);
+			Street street = new Street(points);
 			streets.add(street);
 			// add neighboring nodes to each node
 			if (points.size() > 1) {
