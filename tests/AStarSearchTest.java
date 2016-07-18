680dc6a57da25c0979187c7680e30934566233b1
gibsonhladky <hladky@wisc.edu>
2016-07-18 10:38:53 -0500

    Expanded AStarSearchTest

--- tests/AStarSearchTest.java
+++ tests/AStarSearchTest.java
@@ -18,48 +18,64 @@ public class AStarSearchTest {
 	AStarSearch search;
 	
 	Point start = new Point(0, 0);
-	Point end = new Point(0, 1);
+	Point end = new Point(10, 0);
+	
+	List<Point> noRoute;
+	List<Point> shortRoute;
+	List<Point> longCircuitousRoute;
+	List<Point> longJaggedRoute;
+	List<Point> deadEndRoute;
 
 	@Before
 	public void setUp() throws Exception {
 		search = new AStarSearch(start, end, 2);
+		initializeRoutes();
 	}
 
 	@Test
-	public void disconnectedMapResultInNoRoute() {
-		givenMapContainingRoute(noRoute());
+	public void disconnectedMap() {
+		givenMapContainingRoute(noRoute);
 		
 		whenSearched();
 		
-		thenRouteShouldBe(Collections.emptyList());
+		thenRouteShouldBe(noRoute);
 	}
 	
 	@Test
-	public void nonBranchingMapResultsInProperRoute() {
-		givenMapContainingRoute(shortRoute());
+	public void nonBranchingMap() {
+		givenMapContainingRoute(shortRoute);
 		
 		whenSearched();
 		
-		thenRouteShouldBe(shortRoute());
+		thenRouteShouldBe(shortRoute);
 	}
 	
 	@Test
-	public void branchingMapResultsInProperRoute() {
-		givenMapContainingRoute(shortRoute());
-		givenMapContainingRoute(longRoute());
+	public void branchingMap() {
+		givenMapContainingRoute(shortRoute);
+		givenMapContainingRoute(longCircuitousRoute);
+		givenMapContainingRoute(longJaggedRoute);
 		
 		whenSearched();
 		
-		thenRouteShouldBe(shortRoute());
+		thenRouteShouldBe(shortRoute);
+	}
+	
+	@Test
+	public void deadEndMap() {
+		givenMapContainingRoute(deadEndRoute);
+		givenMapContainingRoute(longCircuitousRoute);
+		
+		whenSearched();
+		
+		thenRouteShouldBe(longCircuitousRoute);
 	}
 	
 	private void givenMapContainingRoute(List<Point> route) {
-		if(route.isEmpty()) {
-			return;
-		}
-		
-		for(int i = 0; i < route.size() - 1; i++) {
-			setAsNeighbors(route.get(i), route.get(i + 1));
+		if(!route.isEmpty()) {
+			for(int i = 0; i < route.size() - 1; i++) {
+				setAsNeighbors(route.get(i), route.get(i + 1));
+			}
 		}
 	}
 	
@@ -76,16 +92,39 @@ public class AStarSearchTest {
 		assertEquals(expectedRoute, search.getRoute());
 	}
 	
-	private List<Point> noRoute() {
-		return Collections.emptyList();
+	
+	private void initializeRoutes() {
+		initializeNoRoute();
+		initializeShortRoute();
+		initializeLongCircuitousRoute();
+		initializeLongJaggedRoute();
+		initializeDeadEndRoute();
 	}
 	
-	private List<Point> shortRoute() {
-		return Arrays.asList(start, end);
+	private void initializeNoRoute() {
+		noRoute = Collections.emptyList();
 	}
 	
-	private List<Point> longRoute() {
-		return Arrays.asList(start, new Point(10, 10), end);
+	private void initializeShortRoute() {
+		shortRoute = Arrays.asList(start, end);
 	}
+	
+	private void initializeLongCircuitousRoute() {
+		longCircuitousRoute = Arrays.asList(start, new Point(5, 5), end);
+	}
+	
+	private void initializeLongJaggedRoute() {
+		longJaggedRoute = new ArrayList<Point>(10);
+		longJaggedRoute.add(start);
+		for(int i = 1; i < 10; i++) {
+			longJaggedRoute.add(new Point(i, i%2));
+		}
+		longJaggedRoute.add(end);
+	}
+	
+	private void initializeDeadEndRoute() {
+		deadEndRoute = Arrays.asList(start, new Point(0, 5));
+	}
+	
 
 }
